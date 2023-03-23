d63f120b9d5b342a2a4b2088cc5e040670225602
MartinD-Aa <martindaa@hotmail.no>
2023-03-23 16:24:50 +0100

    FRON-9 general layout of game
    Made the general layout of the application with styling

--- monster-front/src/App.tsx
+++ monster-front/src/App.tsx
@@ -3,19 +3,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='game'>
+      <section id="monster" className="container">
+        <h2>Monster Health</h2>
+        <div className="healthbar">
+          <div className="healthbar__value"></div>
+        </div>
+      </section>
+      </div>
+      <section id="player" className="container">
+        <h2>Your Health</h2>
+        <div className="healthbar">
+          <div className="healthbar__value" ></div>
+        </div>
+      </section>
+      <section className="container" v-if="winner">
+        <h2>Game Over!</h2>
+        <h3 v-if="winner === 'monster'">You lost!</h3>
+        <h3 v-else-if="winner === 'player'">You won!</h3>
+        <h3 v-else>It's a draw!</h3>
+        <button>Start New Game</button>
+      </section>
+      <section id="controls" v-else>
+        <button >ATTACK</button>
+        <button >
+          SPECIAL ATTACK
+        </button>
+        <button >HEAL</button>
+        <button >SURRENDER</button>
+      </section>
+      <section id="log" className="container">
+        <h2>Battle Log</h2>
+      </section>
     </div>
   );
 }
