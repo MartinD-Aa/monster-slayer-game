1550c9b0c676cbdad98508af6dbd8b24ba164097
MartinD-Aa <martindaa@hotmail.no>
2023-03-24 12:25:07 +0100

    Split elements into components

---
+++ monster-front/src/components/GameOver.tsx
@@ -0,0 +1,14 @@
+function GameOver () {
+    return(
+        <section className="container">
+        <h2>Game Over!</h2>
+        <h3 >You lost!</h3>
+        <h3 >You won!</h3>
+        <h3>You ran away!</h3>
+        <h3 >It's a draw!</h3>
+        <button>Start New Game</button>
+      </section>
+
+    );
+}
+export default GameOver;
